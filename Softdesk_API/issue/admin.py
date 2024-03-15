b03b3312da70adbbd6b6388bc565a9a8498e2e01
geo1310 <gbriche59@yahoo.fr>
2024-03-15 13:14:33 +0100

    Creation IssueAdmin

--- Softdesk_API/issue/admin.py
+++ Softdesk_API/issue/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Issue
+
+
+@admin.register(Issue)
+class IssueAdmin(admin.ModelAdmin):
+    """
+    Administration personnalisée pour le modèle Project.
+
+    Attributes:
+        list_display (tuple): Liste des champs à afficher dans la liste des utilisateurs de l'interface admin.
+    """
+
+    list_display = (
+        "author",
+        "title",
+        "type",
+        "project_assigned",
+        "priority",
+        "progress",
+        "id",
+    )
